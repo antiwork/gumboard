23d95eb6c3d581ae0cc0dbbabf75b78c2342c5b2
Fahad <fahad.khan2216@gmail.com>
2025-08-07 00:38:54 +0530

    updated auth.ts

--- auth.ts
+++ auth.ts
@@ -2,10 +2,12 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import Resend from "next-auth/providers/resend"
 import GoogleProvider from "next-auth/providers/google"
-import { db } from "./lib/db"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(db),
+  adapter: PrismaAdapter(prisma),
   providers: [
     Resend({
       from: process.env.EMAIL_FROM!,
