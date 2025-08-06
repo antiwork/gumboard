6cda47c8907a0ae4a0c222d944870d6daa2af279
Nalin-khanna <nalinkhanna17@gmail.com>
2025-08-07 00:27:06 +0530

    allow email account linking

--- auth.ts
+++ auth.ts
@@ -15,6 +15,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   pages: {
