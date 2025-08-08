02b6d6daa9c0ebb442f43403802f9421264257a7
gurshaan17 <gurshaansinghd@gmail.com>
2025-08-09 00:57:53 +0530

    removed the isChecklist field

---
+++ prisma/migrations/20250808192721_remove_is_checklist_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isChecklist` on the `notes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "notes" DROP COLUMN "isChecklist";
