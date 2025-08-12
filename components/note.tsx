199675bf6d349759bcf38962847256e498fdcc21
Lucky negi <luckynegi1025@gmail.com>
2025-08-13 02:40:38 +0530

    small_fix

--- components/note.tsx
+++ components/note.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import {
   ChecklistItem as ChecklistItemComponent,
