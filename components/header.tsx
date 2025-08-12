abcf0efa9aead85a78d4100f0fb23977d98a063d
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-08-13 04:05:22 +0530

    fix: added sticky in header

--- components/header.tsx
+++ components/header.tsx
@@ -12,7 +12,7 @@ interface HeaderProps {
 
 export function Header({ user, leftContent, rightContent }: HeaderProps) {
   return (
-    <div className="bg-card dark:bg-zinc-900 border-b border-gray-200 dark:border-zinc-800 shadow-sm">
+    <div className="bg-card sticky top-0 z-50 dark:bg-zinc-900 border-b border-gray-200 dark:border-zinc-800 shadow-sm">
       <div className="flex justify-between items-center h-16 px-4 sm:px-2 md:px-4">
         <div className="flex items-center space-x-4">
           <Link href="/dashboard" className="flex-shrink-0 pl-4 sm:pl-2 lg:pl-4">
