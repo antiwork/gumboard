2fe2d4958bd9da20a034d70b9c5abe2b47593d0b
Ayush Sharma <ayush.sharma74892@gmail.com>
2025-08-16 00:05:43 +0530

    fix(ui): remove hover bg from note checklist

--- components/note.tsx
+++ components/note.tsx
@@ -457,7 +457,7 @@ export function Note({
           >
             <DraggableContainer className="space-y-1">
               {note.checklistItems?.map((item) => (
-                <DraggableItem key={item.id} id={item.id}>
+                <DraggableItem key={item.id} id={item.id} className="hover:bg-transparent">
                   <ChecklistItemComponent
                     item={item}
                     onToggle={handleToggleChecklistItem}
