f995157879be45979526d6d43f15b6804cf8d9fc
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-08-20 18:07:48 +0530

    fix tests

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="min-h-screen flex flex-col items-center justify-center text-center px-4x">
       <h1 className="text-7xl font-extrabold dark:text-white">404</h1>
       <p className="mt-4 text-xl text-gray-700 dark:text-white/80">
-        The page you’re looking for doesn’t exist.
+        Page not found
       </p>
       <div className="mt-6">
         <Button asChild>
