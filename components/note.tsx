632235f2ff88cd6ab205b11ef68c8ef577b95904
Anshgrover23 <ag5989670@gmail.com>
2025-09-12 18:40:04 +0530

    fix failing tests

--- components/note.tsx
+++ components/note.tsx
@@ -346,6 +346,7 @@ export function Note({
         "before:bg-gradient-to-b before:from-black/5 before:to-transparent dark:before:from-white/5",
         className
       )}
+      data-testid="note-card"
       aria-label={`Note ${note.id}`}
       onFocusCapture={() => {}}
       onBlurCapture={() => {}}
